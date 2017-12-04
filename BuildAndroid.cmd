2e5e2fc0db459c359fd0c20563298e2780155e60
anydream <anydreamplay@hotmail.com>
2017-12-04 16:41:25 +0800

    ~: Add build scripts

---
+++ BuildAndroid.cmd
@@ -0,0 +1,6 @@
+cd /D src
+set GOROOT_BOOTSTRAP=D:\go
+set CGO_ENABLED=0
+set GOOS=android
+set GOARCH=arm
+make.bat
---
+++ BuildGo.cmd
@@ -0,0 +1,4 @@
+cd /D src
+set GOROOT_BOOTSTRAP=D:\go
+set CGO_ENABLED=0
+make.bat
